e6ddad358e76f9326fcb4d294853362b6505447d
Maikel Maas <mmaas@sphereon.com>
2021-08-20 16:16:54 +0200

    ESSIFI-98 Create example requests
       - Setup endpoints with dummy data;

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import express, { NextFunction, Request, Response } from 'express';
-import compression from 'compression';
-import { TEST_CONTROLLER } from './controllers/test-controller';
-import { Server } from 'http';
+import compression from 'compression'; 
+import { THREAD_CONTROLLER } from './controllers/threadController';
+import { PRESENTATION_CONTROLLER } from './controllers/presentationController';
+import { DEFINITIONS_CONTROLLER } from './controllers/definitionController';
 
 const APP: express.Application = express();
-const PORT = process.env.PORT || 3000;
-const ENV = process.env.ENV || 'develepment';
 
 APP.use(compression());
 APP.use(express.json());
 APP.use(express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 3000;
+const ENV =  process.env.ENV;
 
 APP.use((req: Request, res: Response, next: NextFunction) => {
   console.log(req.url);
   next();
 })
 
-APP.use('/', TEST_CONTROLLER);
+APP.use('/pe/v1', THREAD_CONTROLLER);
+APP.use('/pe/v1', PRESENTATION_CONTROLLER);
+APP.use('/pe/v1', DEFINITIONS_CONTROLLER);
 
-const SERVER: Server = APP.listen(PORT, async () => {
+APP.listen(PORT, async () => {
   console.log(
       "App is running at http://localhost:%d in %s mode",
       PORT,
